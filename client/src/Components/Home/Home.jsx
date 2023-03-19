3a50ff5e2099e9d0d83eb077953d23267a9d4190
Rzyfront <rzyfront@gmail.com>
2023-03-19 02:56:27 -0500

    Landing completo, Home incompleto

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,59 +1,118 @@
-import React from 'react'
-import { IoFilterCircleOutline, IoFilterCircleSharp } from 'react-icons/io5';
-import { useState,useEffect } from 'react';
-import { useDispatch,useSelector  } from "react-redux";
-import { getAllVideogames } from '../../Redux/actions.js';
-
+import React from "react";
+import { IoFilterCircleOutline, IoFilterCircleSharp } from "react-icons/io5";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllVideogames, getGenres } from "../../Redux/actions.js";
+import { NavLink } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   const [toggleFilter, setToggleFilter] = useState();
   const Games = useSelector((state) => state.allVideogames);
+  const Genres = useSelector((state) => state.allGenres);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (!Games) {
       dispatch(getAllVideogames());
+      dispatch(getGenres());
     }
   });
 
+  const handleSort = () => {};
+
+  const handleRatingSort = () => {};
+
+  const handleGenres = () => {};
+
+  const handleFilterCreated = () => {};
+
   return (
-    <div className='Home'>
-      <div className='Filters'>
-  
-
-
-        <div className='filterMenu'>
-          {toggleFilter
-          ? <IoFilterCircleOutline color='fff' size={27} onClick={() => setToggleFilter(false)}/>
-          : <IoFilterCircleSharp color='fff' size={27} onClick={() => setToggleFilter(true)}/>
-        }
-        {toggleFilter && (
-          <div className="filterMenu-toggle">
-            <div className="filters">
-              
-            
+    <div className="Home">
+      <div className="Filters">
+        <div className="filterMenu">
+          {toggleFilter ? (
+            <IoFilterCircleOutline
+              color="fff"
+              size={27}
+              onClick={() => setToggleFilter(false)}
+            />
+          ) : (
+            <IoFilterCircleSharp
+              color="fff"
+              size={27}
+              onClick={() => setToggleFilter(true)}
+            />
+          )}
+          {toggleFilter && (
+            <div className="filterMenu-toggle">
+              <div className="filters"></div>
             </div>
-          </div>
-          
-        )}
-
+          )}
         </div>
       </div>
-      <div className='renderCards'>
-        {
-          Games &&
-          Games.map((game) => {
-            return <Card
-            
-            
-            
-            
-            />
-          }) 
-        }
+
+      <div className="renderCards">
+        <section>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="box">
+            <div className="square" style={{ "--i": "0" }}></div>
+            <div className="square" style={{ "--i": "1" }}></div>
+            <div className="square" style={{ "--i": "2" }}></div>
+            <div className="square" style={{ "--i": "3" }}></div>
+            <div className="square" style={{ "--i": "4" }}></div>
+            <div className="container">
+              <div className="nav">
+                <div className="links">
+                  <h2>NEXUS</h2>
+                  <input
+                    type="text"
+                    name="search"
+                    placeholder="Busca tus juegos favoritos aqui..."
+                  />
+                  <button>Buscar</button>
+                </div>
+              </div>
+              <div className="filters">
+                <NavLink>
+                  <button>Crear Juego +</button>
+                </NavLink>
+                <div className="selectores">
+                  <select onChange={(e) => handleSort(e)}>
+                    <option value="Order-Alphabetical">
+                      Order from the...
+                    </option>
+                    <option value="A-Z"> A-Z </option>
+                    <option value="Z-A"> Z-A </option>
+                  </select>
+                  <select onChange={(e) => handleRatingSort(e)}>
+                    <option value="Order-Rating">Order by rating</option>
+                    <option value="Men-May">Men-May</option>
+                    <option value="May-Men">May-Men</option>
+                  </select>
+                  <select onChange={(e) => handleGenres(e)}>
+                    <option value="All">Genres</option>
+                    {Genres?.map((el) => (
+                      <option key={el.id} value={el.name}>
+                        {el.name}
+                      </option>
+                    ))}
+                  </select>
+                  <select onChange={(e) => handleFilterCreated(e)}>
+                    <option value="All">All</option>
+                    <option value="Created">Created in DB</option>
+                    <option value="Existing">From the API</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { useEffect } from "react";
+import React from "react";
+import videoMp4 from "../../Assets/Landing.mp4";
+import videoWebm from "../../Assets/Landing.webm";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch } from "react-redux";
-import { getAllVideogames, getGenres } from '../../Redux/actions.js';
+import { getAllVideogames, getGenres } from "../../Redux/actions.js";
+import "./Landing.css";
+import { NavLink } from "react-router-dom";
 
 const Landing = () => {
   const dispatch = useDispatch();
+  const [isH2Visible, setIsH2Visible] = useState(false);
+  const h2Ref = useRef(null);
+  useEffect(() => {
+    const h2 = h2Ref.current;
+    const position = h2.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 2;
+    const isH2Visible = position < screenPosition;
+    setIsH2Visible(isH2Visible);
+  }, []);
 
   useEffect(() => {
     dispatch(getAllVideogames());
@@ -12,8 +25,33 @@ const Landing = () => {
   });
 
   return (
-    <div><h1>BIENVENIDO SOY EL LANDING PAGEEEEEE</h1></div>
-  )
-}
+    <div className="Landing">
+      <video autoPlay loop muted>
+        <source src={videoMp4} type="video/mp4" />
+        <source src={videoWebm} type="video/webm" />
+      </video>
 
-export default Landing
+      <div className="content">
+        <div className="bodyLanding">
+          <div className="containerLanding">
+            <h2
+              ref={h2Ref}
+              className={`glitch animate-h2 ${
+                isH2Visible ? "animate-h2-active" : ""
+              }`}
+            >
+              DESCUBRE LA MEJOR WEB GAMING
+            </h2>
+            <div className="btn">
+              <NavLink to={"/Home"} className="navlink">
+                EMPEZAR
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
